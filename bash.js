9d4504fc2c447d6a81f913286240760271570ebd
Mario Carbache <mariocarbache@gmail.com>
2023-07-10 19:39:30 -0400

    added ls.js file

--- bash.js
+++ bash.js
@@ -1,4 +1,4 @@
-const pwd = require("./pwd")
+const pwd = require("./pwd");
 process.stdout.write('prompt > ');
 
 // The stdin 'data' event fires after a user types in a line
@@ -6,6 +6,9 @@ process.stdin.on('data', (data) => {
   const cmd = data.toString().trim(); // remove the newLine
   if (cmd == "pwd"){
     pwd();
+  }
+  if (cmd == "ls"){
+    const ls = require("./ls");
   }
   process.stdout.write('\nprompt > ');
 });
